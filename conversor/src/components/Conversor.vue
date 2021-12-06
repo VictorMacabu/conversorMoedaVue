4d69e50ab71cff6a57f13b6e30c59f61655ac194
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-12-06 18:15:38 -0300

    add API busca

--- conversor/src/components/Conversor.vue
+++ conversor/src/components/Conversor.vue
@@ -2,11 +2,9 @@
     <div class="conversor"> 
 
         <h2>{{moeda1}} Para {{moeda2}}</h2>
-        <input type="text" v-model="moeda1_value" v-bind:placeholder="moeda1"/>
-        <input type="button" value="Converter"/>
+        <input type="text" v-model="moeda1_value" v-bind:placeholder="moeda1">
+        <input type="button" value="Converter" v-on:click="converter" >
         <h2>{{moeda2_value}}</h2>
-
-
 
     </div>
 </template>
@@ -28,6 +26,14 @@ export default {
             let de_para = this.moeda1 + "_" + this.moeda2;
 
             let url = "https://free.currconv.com/api/v7/convert?q="+ de_para +"&compact=ultra&apiKey=9e4a08e074354e2c273b";
+        fetch(url).then(resposta => {return resposta.json()})
+                        .then(json =>{
+                            let cotacao = json[de_para];
+                            parseFloat(cotacao);
+                            console.log(cotacao);
+                            
+                            this.moeda2_value = (cotacao * parseFloat(this.moeda1_value)).toFixed(2);
+                        })
         }
     }
 };
