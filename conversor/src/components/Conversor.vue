56fbea7f233d21bd64131b1aef7072c176725f85
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-12-06 14:25:58 -0300

    start

---
+++ conversor/src/components/Conversor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="conversor"> 
+
+        <h2>{{moeda1}} Para {{moeda2}}</h2>
+        <input type="text" v-model="moeda1_value" v-bind:placeholder="moeda1"/>
+        <input type="button" value="Converter"/>
+        <h2>{{moeda2_value}}</h2>
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+ name: "Conversor",
+ props: ["moeda1","moeda2"],
+
+    data(){
+        return{
+            moeda1_value: "",
+            moeda2_value: 0
+        };
+    },
+    methods:{
+        converter(){
+            let de_para = this.moeda1 + "_" + this.moeda2;
+
+            let url = "https://free.currconv.com/api/v7/convert?q="+ de_para +"&compact=ultra&apiKey=9e4a08e074354e2c273b";
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
